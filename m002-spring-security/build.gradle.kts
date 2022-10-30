bcb3b715bf227fd21e69ab35ffafd0a6d32cbba1
Worrovsky <Worrovsky@users.noreply.github.com>
2022-10-30 23:55:03 +0300

    Add Spring Security sample

---
+++ m002-spring-security/build.gradle.kts
@@ -0,0 +1,11 @@
+dependencies {
+
+//    implementation("org.springframework.boot:spring-boot-starter")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,5 @@
 rootProject.name = "p004-spring-misc"
 
 include("m001-scheduling")
+include("m002-spring-security")
 
